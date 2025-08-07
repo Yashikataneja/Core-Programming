9b40989db8273a71c045676f63e64b5c3f1f0f3f
YashikaTaneja <yashika979.be22@chitkara.edu.in>
2025-08-07 05:58:21 +0530

    new files of stringg

---
+++ EXTRAS-Java_String_Handling/day8_problems/CountVowelsConsonants.java
@@ -0,0 +1,13 @@
+public class CountVowelsConsonants {
+    public static void main(String[]args){
+        //count the no. of vowels and consonants in a string
+        String s="yashi";
+        System.out.println("the string is : "+s);
+
+        //vowels : a , e , i , o , u
+        //rest of others are  consonants
+
+
+
+    }
+}
