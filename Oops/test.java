91d4479fc3e5c94d5dca30d7f0a7e8b53f0e1847
YashikaTaneja <yashika979.be22@chitkara.edu.in>
2025-08-22 09:48:45 +0530

    new files

---
+++ Oops/test.java
@@ -0,0 +1,12 @@
+class Samsung {
+    Samsung s=new Samsung();
+    Samsung(){
+        System.out.println("galaxy g25 ultra");
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        Samsung k = new Samsung();
+    }
+}
